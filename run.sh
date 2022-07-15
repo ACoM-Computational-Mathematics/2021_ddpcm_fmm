ad65b3be3b3a4df462a3645e915d2f175531bdad
Aleksandr Mikhalev <al.mikhalev@skoltech.ru>
2022-07-15 15:21:08 +0300

    Changes in plotting script

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
-cd /home/michele/git_repo/2021_ddpcm_fmm
 # Set parallelism level
-j=20
+j=10
 # Run dense_vs_fmm tests
 make -C experiments/dense_vs_fmm_vdw -j$j
-## Run fmm_vs_fmm tests
-# make -C experiments/fmm_vs_fmm_vdw -j$j
+# Run fmm_vs_fmm tests
+make -C experiments/fmm_vs_fmm_vdw -j$j
+# Run large tests
+make -C experiments/large_fmm_vdw -j$j
